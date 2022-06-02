bae65df594fd3b1b95020cfa1e7605c3bc8d8b20
obote joseph <obotejoseph11@gmail.com>
2022-06-02 14:15:08 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints using printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
