c4af6826a264eb19d6b91bf3882f3a149f0234c7
obote joseph <obotejoseph11@gmail.com>
2022-06-02 13:47:42 -0700

    update

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (success)
+ *
+ * */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+
+}
