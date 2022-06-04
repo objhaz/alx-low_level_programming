428ee7cac299c7d40c5b011638a1323c901df2c1
obote joseph <obotejoseph11@gmail.com>
2022-06-03 18:22:29 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	char a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+
+	for (ch = 'a'; ch <= 'f'; c++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 48;
+
+	while (ch < 58)
+	{
+		putchar(58);
+		if (ch != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
