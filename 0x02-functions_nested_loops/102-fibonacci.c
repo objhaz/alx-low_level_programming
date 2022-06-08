9f8e5e6085762655024cb4bbfd8c3626f8166da6
obote joseph <obotejoseph11@gmail.com>
2022-06-07 19:19:52 -0700

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-	long int a, b, s, i;
+	long int a, b, num, i;
 
 	a = 1;
 	b = 2;
-	s = 3;
-	printf("%d, ", a);
-	printf("%d, ", b);
+	num = 3;
+	printf("%ld, ", a);
+	printf("%ld, ", b);
 	for (i = 3; i < 50; i++)
 	{
-		printf("%d, ", s);
+		printf("%ld, ", num);
 		a = b;
-		b = s;
-		s = a + b;
+		b = num;
+		num = a + b;
 	}
-	printf("%d\n", s);
+	printf("%ld\n", num);
 
 	return (0);
 }
