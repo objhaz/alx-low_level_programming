a12e0e9d947e4d1860cdeb91a25511a53ed23af2
obote joseph <obotejoseph11@gmail.com>
2022-06-09 19:16:39 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - function that multiplies two integers
+* @a: takes in an integer
+* @b: takes in an integer
+* Return: the result from multiplying two integers
+**/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks done on more functions and nested loops
